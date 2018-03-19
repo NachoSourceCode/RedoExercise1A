380bb67ee177fd97f49f97b50b00b50b428553bd
Matthew Slone <Matthew.Slone@outlook.com>
2018-03-19 12:20:59 -0400

    calculate volume of  a hemisphere, area of a triangle, quadratic equation, and prints results for all calculations.

--- AreaAndCircumferenceOfCircle/Program.cs
+++ AreaAndCircumferenceOfCircle/Program.cs
@@ -25,17 +25,32 @@ namespace AreaAndCircumferenceOfCircle
         static void Main(string[] args)
         {
 
-           int radius = 20;
+            int radius = 20;
+            int denominator = 2;
+            int a = 3;
+            int b = 4;
+            int c = 3;
+            int aa = 1;
+            int bb = 6;
+            int cc = -16;
 
-           Program aoc = new Program();
-           Program coc = new Program();
+            Program aoc = new Program();
+            Program coc = new Program();
+            Program voh = new Program();
+            Program aot = new Program();
+            Program qe = new Program();
 
-           Console.WriteLine(aoc.AreaOfCircle(radius));
-           Console.WriteLine(coc.CircumOfCircle(radius));
+            Console.WriteLine($"Area of a circle: {aoc.AreaOfCircle(radius)}");
+            Console.WriteLine($"Circumference of a circle: {coc.CircumOfCircle(radius)}");
+            Console.WriteLine($"Volume of a hemisphere: {voh.VolOfHemisphere(radius, denominator)}");
+            Console.WriteLine($"Area of a triangle: {aot.AreaOfTriangle(a, b, c, denominator)}");
+
+            var res = qe.QuadraticEquation( aa, bb, cc);
+            Console.WriteLine($"Quadratic coordinates: {res[0]}, {res[1]}");
 
         }
 
-        public double AreaOfCircle(double radius)
+        public double AreaOfCircle(int radius)
         {
             
             double area = Math.PI * Math.Pow(radius, 2);
@@ -44,7 +59,7 @@ namespace AreaAndCircumferenceOfCircle
 
         }
 
-        public double CircumOfCircle(double radius)
+        public double CircumOfCircle(int radius)
         {
 
             double circum = 2 * Math.PI * radius;
@@ -53,6 +68,42 @@ namespace AreaAndCircumferenceOfCircle
 
         }
 
+        public double VolOfHemisphere(int radius, int denominator)
+        {
+            double volume = (1.33 * Math.PI * Math.Pow(radius, 3)) / denominator;
 
+            return volume;
+        }
+        
+        public double AreaOfTriangle(int a, int b, int c, int denominator)
+        {
+
+            //calculate half the circumference of a triangle(semi parameter)
+            double sp = (a + b + c) / denominator;
+
+            //calculate area of circle
+            double triangleArea = Math.Sqrt(sp * (sp -a) * (sp -b) * (sp - c));
+
+            return triangleArea;
+        }
+
+        public double[] QuadraticEquation(int aa, int bb, int cc)
+        {
+            int denominatorQuadratic = (2 * aa);
+
+
+            double underSqrt = Math.Pow(bb, 2) - (4 * aa * cc);
+
+            double left = (-bb) / denominatorQuadratic;
+            double right = Math.Sqrt(underSqrt) / denominatorQuadratic;
+            double x1 = left + right;
+            double x2 = left - right;
+            double[] answer = new double[2];
+            answer[0] = x1;
+            answer[1] = x2;
+
+            return answer;
+            
+        }
     }
 }
